191fc1c431b39a2f073fc14e441ce3bec6321f4e
savan2004 <savan2004@gmail.com>
2025-12-18 16:37:27 +0530

    FIX: Module 3 - Switch to TestApp to test React rendering

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TestApp from './TestApp';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
@@ -11,6 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <TestApp />
   </React.StrictMode>
 );
